bd7add06d5abdc54a01de6683292c9236aca164d
zhjmgmj <943257156@qq.com>
2021-03-12 11:02:40 +0800

    'first'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+div
+  span 2222{{count}}{{num}}
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup(props) {
+    const count = ref(1)
+    console.log('----------setUp', props)
+    return {count}
+  },
+  data() {
+    return{
+      num: 0
+    }
+  },
+  mounted() {
+    console.log('count', this.count++)
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
